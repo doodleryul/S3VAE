1b8faa78dfcca0473216d0144e54dea5d916ec99
YURIM <rimiiii.u@gmail.com>
2023-02-01 14:42:42 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='s3vae',
+    packages=find_packages(),
+    version='0.1',
+    author='doodleryul',
+    author_email='rimiiii.u@gmail.com',
+    license='MIT',
+    description='S3VAE Implementation',
+    long_description=open('README.md').read(),
+    url='https://github.com/doodleryul/S3VAE'
+)
